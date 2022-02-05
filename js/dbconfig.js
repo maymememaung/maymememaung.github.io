a5248c6c61746563b6547248533147747f4fab0f
May Me Me Maung <maymememaung27@gmail.com>
2022-02-05 18:48:22 -0500

    Firebase v4

--- js/dbconfig.js
+++ js/dbconfig.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyBMsvknmUwQ7d1SWtVBbx_st7iAla5aA0k",
     authDomain: "personal-portfolio-956e1.firebaseapp.com",
     projectId: "personal-portfolio-956e1",
-    storageBucket: "gs://personal-portfolio-956e1.appspot.com",
+    storageBucket: "personal-portfolio-956e1.appspot.com",
     messagingSenderId: "408821020828",
     appId: "1:408821020828:web:1b5b564d6c6dee4faf9559",
     measurementId: "G-X3DVYN05FW"
